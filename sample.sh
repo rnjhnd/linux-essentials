f94c783e08caf8267898665607d38595b3da4bed
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2025-02-05 01:43:25 +0000

    Save File

---
+++ sample.sh
@@ -0,0 +1,13 @@
+echo "Enter your name:"
+read NAME
+echo ""
+
+echo "Enter your age:"
+read AGE
+echo ""
+
+echo "Enter your program:"
+read PROGRAM
+echo ""
+
+echo "Hello, $NAME! You are $AGE yrs. old & your program is $PROGRAM."
