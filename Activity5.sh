b3c782020d065455026ac5dcbd9892952628aa6c
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2025-03-20 07:10:00 +0000

    Update Files

---
+++ Activity5.sh
@@ -0,0 +1,43 @@
+: '
+echo "#1: Print numbers from 1 to 10"
+
+number=1
+while [ "$number" -le 10 ]; do
+   echo "Number $number"
+   number=$((number + 1))
+done
+
+echo
+echo "#2: Skip even numbers using continue"
+
+number=1
+
+while [ $number -le 10 ]
+do
+    if [ $((number % 2)) -eq 0 ]
+    then
+        number=$((number + 1))
+        continue
+    fi
+    echo "Odd Number: $number"
+    number=$((number + 1))
+done
+'
+
+
+echo
+echo "#3: Rename .jpg files to .png."
+
+
+for file in *.jpg
+do
+    if [ -f "$file" ]
+    then
+        new_name="${file%.jpg}.png"
+        mv "$file" "$newname"
+        echo "Renamed $file to $new_name"
+    else
+        echo "No .jpg files found."
+    fi
+done
+
